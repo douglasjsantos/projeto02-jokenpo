01f0acad337a67c6f0107774306f0fe984975403
Douglas Santos <douglas_j.santos@hotmail.com>
2023-02-23 19:48:10 -0300

    adicionado layout e novas funcionalidades

--- script.js
+++ script.js
@@ -1,43 +1,55 @@
-function getComputerChoice() {
+const botoes = document.querySelectorAll("button");
+const span = document.getElementById("vc");
+const span2 = document.getElementById("cpu");
+
+let contadorUsuario = 0;
+let contadorPC = 0;
+let partidas = 0;
+
+const respostaPc = function getComputerChoice() {
   const ppt = ["Pedra", "Papel", "Tesoura"];
   const pptNumber = Math.floor(Math.random(ppt) * 3);
   return ppt[pptNumber].toUpperCase();
-}
+};
 
-function playRound(playerSelection, computerSelection) {
-  if (playerSelection == "PEDRA" && computerSelection == "TESOURA") {
-    return `Você ganhou! Pedra ganha de Tesoura`;
-  } else if (playerSelection == "PEDRA" && computerSelection == "PAPEL") {
-    return `Você perdeu! Pedra perde de Tesoura`;
-  }
-  if (playerSelection == "TESOURA" && computerSelection == "PAPEL") {
-    return `Você ganhou! Tesoura ganha de Papel`;
-  } else if (playerSelection == "TESOURA" && computerSelection == "PEDRA") {
-    return `Você perdeu! Tesoura perde para Pedra`;
-  }
-  if (playerSelection == "PAPEL" && computerSelection == "PEDRA") {
-    return `Você ganhou! Papel ganha de Pedra`;
-  } else if (playerSelection == "PAPEL" && computerSelection == "Tesoura") {
-    return `Você perdeu! Papel perde para Tesoura`;
-  }
-  if (playerSelection == computerSelection) {
-    return `Empate`;
-  }
-}
+const atualizaPlacar = function () {
+  span.innerHTML = contadorUsuario;
+  span2.innerHTML = contadorPC;
+};
 
-let playerSelection;
+const reiniciarPlacar = function () {
+  contadorUsuario = 0;
+  contadorPC = 0;
+  partidas = 0;
+  atualizaPlacar();
+};
 
-while (
-  playerSelection !== "TESOURA" &&
-  playerSelection !== "PEDRA" &&
-  playerSelection !== "PAPEL"
-) {
-  playerSelection = prompt("Digite: Pedra, Papel ou Tesoura")
-    .toUpperCase()
-    .trim();
-}
-
-alert("Boa sorte!");
-
-const computerSelection = getComputerChoice();
-console.log(playRound(playerSelection, computerSelection));
+botoes.forEach((item) => {
+  item.addEventListener("click", (e) => {
+    const usuario = e.target.innerHTML.toUpperCase();
+    const pc = respostaPc();
+    if (usuario === pc) {
+      contadorUsuario++;
+      contadorPC++;
+      if (contadorUsuario === 1) {
+        atualizaPlacar();
+      }
+    } else {
+      if (usuario === "PEDRA" && pc === "TESOURA") {
+        contadorUsuario++;
+      } else if (usuario === "PAPEL" && pc === "PEDRA") {
+        contadorUsuario++;
+      } else if (usuario === "TESOURA" && pc === "PAPEL") {
+        contadorUsuario++;
+      } else {
+        contadorPC++;
+      }
+      partidas++;
+      if (contadorUsuario === 5 || contadorPC === 5) {
+        reiniciarPlacar();
+      } else {
+        atualizaPlacar();
+      }
+    }
+  });
+});
